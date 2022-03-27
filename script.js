7f85c4ed48f17de86ecb657150dcd8038a498cb7
Pulupenko Alexey <optycat@gmail.com>
2022-03-27 21:50:44 +0300

    auto request & checkout

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const numberOfFilms = prompt("how many films have you watched?", "");
+'use strict';
+const numberOfFilms = +prompt("how many films have you watched?", "");
 
 let personalMovieDB = {
     count: numberOfFilms,
@@ -7,7 +8,79 @@ let personalMovieDB = {
     genres: [],
     private: false
 };
-
-personalMovieDB.movies[prompt("Name of one of the last films, you watched?", "")] = prompt("Yours feedback from 1 to 10?", "");
+for( let i = 0; i < 2; i++ ){
+    let a, b;
+    a = prompt("Name of one of the last films, you watched?", "");
+    b = prompt("Yours feedback from 1 to 10?", "");
+    if(a != "" && b != "" && a != null && b != null && a.length < 50){
+        console.log("done!");
+        personalMovieDB.movies[a] = b;
+        //a = prompt("Re-enter name of film, field require smht", "");
+    } else {
+        console.log("Error!");
+        i--;
+    }
+    /*if(a == ""){
+        a = prompt("Re-enter name of film, field require smht", "");
+        i--;
+    }else if(b == ""){
+        b = prompt("Re-enter feedback from 1 to 10, field require smht", "");
+        i--;
+    } else if(a == null){
+        a = prompt("Re-enter name of film, field require smht", "");
+        i--;
+    } else if(b == null){
+        b = prompt("Re-enter feedback from 1 to 10, field require smht", "");
+        i--;
+    } else if(a.length < 50){
+        a = prompt("Re-enter name of film, field require smht", "");
+        i--;
+    } else if(b.length < 50){
+        b = prompt("Re-enter feedback from 1 to 10, field require smht", "");
+        i--;
+    }
+    console.log("a= ", a);
+    console.log("b= ", b);
+    personalMovieDB.movies[a] = b;*/
+}
+/*
+let i = 0;
+while( i < 2 ){
+    let a, b;
+    a = prompt("Name of one of the last films, you watched?", "");
+    b = prompt("Yours feedback from 1 to 10?", "");
+    i++;
+    if(a != "" && b != "" && a != null && b != null && a.length < 50){
+        console.log("done!");
+        personalMovieDB.movies[a] = b;
+    } else {
+        console.log("Error!");
+        i--;
+    }
+}
+let i = 0;
+do{
+    let a, b;
+    a = prompt("Name of one of the last films, you watched?", "");
+    b = prompt("Yours feedback from 1 to 10?", "");
+    i++;
+    if(a != "" && b != "" && a != null && b != null && a.length < 50){
+        console.log("done!");
+        personalMovieDB.movies[a] = b;
+    } else {
+        console.log("Error!");
+        i--;
+    }
+}while( i < 2 );
+*/
+if(personalMovieDB.count < 10){
+    console.log("U watched a few films")
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30 ){
+    console.log("U R - classic veuwer")
+} else if (personalMovieDB.count >= 30 ){
+    console.log("U R - cinemaholic")
+} else{
+    console.log("Error!")
+}
 
 console.log(personalMovieDB);
